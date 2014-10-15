ef11189868e7a7989938d7d12062590ac0dba268
peterzd <zdsunshine0640@gmail.com>
2014-10-15 16:35:53 +0800

    [test]first user tests

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,6 @@
 
 FactoryGirl.define do
   factory :user do
+    username "peterzd"
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { create :user, email: "peter@test.com", password: "11111111" }
 
   it "must be valid" do
     user.must_be :valid?
+  end
+
+  it "has a username" do
+    user.username.must_equal "peterzd"
   end
 end
