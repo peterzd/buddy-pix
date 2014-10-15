cdf152a5259df44dacce7fcfce45ef08df65e520
peterzd <zdsunshine0640@gmail.com>
2014-10-15 09:44:48 +0800

    [chore]initialize minitest with spec syntax

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,19 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module PhotoShare
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.view_specs false
+      g.template_engine :haml
+      g.test_framework :minitest, spec: true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
