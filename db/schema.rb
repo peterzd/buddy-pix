3d52a4d3e1b1a9fb0527177c44d5b872db633b9b
peterzd <zdsunshine0640@gmail.com>
2014-10-17 12:47:22 +0800

    [feature]add images model to manage uploaded photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016120323) do
+ActiveRecord::Schema.define(version: 20141017030318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20141016120323) do
   end
 
   add_index "albums", ["creator_id"], name: "index_albums_on_creator_id", using: :btree
+
+  create_table "images", force: true do |t|
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
