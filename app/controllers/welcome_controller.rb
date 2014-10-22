018973169f22a7c3f909479a94606be348ff3b50
peterzd <zdsunshine0640@gmail.com>
2014-10-22 12:34:06 +0800

    [feature]add assets into project
    Note: we need to make the images and CSS folders as the style as Rails
    pipeline

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about_us
+  end
+
+  def terms
+  end
+
+  def blog
+  end
+
+  def support
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   resources :albums
 
   devise_for :users
-  root to: "home#index"
 
   resources :images do
     resources :comments
   end
 
+  root to: "welcome#index"
+  controller "welcome" do
+    get :index
+    get :about_us
+    get :terms
+    get :blog
+    get :support
+  end
 end
