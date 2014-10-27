616bb1c4dfbbf03cb0fd7b2553313d3b79b69558
peterzd <zdsunshine0640@gmail.com>
2014-10-27 21:57:58 +0800

    [chore]finish extract code from function.js to global_effect.js

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -25,24 +25,24 @@ jQuery(function($) {
 
         
     //smooth scroll
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length && $(window).width()>=640) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top + -60
-                }, 1000);
-                return false;
-            }
-/*            else if (target.length && $(window).width() < 640) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top 
-                }, 1000);
-                return false;
-            }*///this codition for mobile screens
-        }
-    });
+    // $('a[href*=#]:not([href=#])').click(function() {
+    //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //         if (target.length && $(window).width()>=640) {
+    //             $('html,body').animate({
+    //                 scrollTop: target.offset().top + -60
+    //             }, 1000);
+    //             return false;
+    //         }
+// /*            else if (target.length && $(window).width() < 640) {
+    //             $('html,body').animate({
+    //                 scrollTop: target.offset().top 
+    //             }, 1000);
+    //             return false;
+    //         }*///this codition for mobile screens
+    //     }
+    // });
     
     //Select Box customization	
     // $('input[type="checkbox"],input[type="radio"]').iCheck({
@@ -52,9 +52,9 @@ jQuery(function($) {
     // });
     
     //option click btn
-    $('.option_drop span').click(function(){
-        $(this).parent().find('ul').slideToggle();
-    });
+    // $('.option_drop span').click(function(){
+    //     $(this).parent().find('ul').slideToggle();
+    // });
     
     
     //Select Box customization	
@@ -67,36 +67,36 @@ jQuery(function($) {
 
     /***************** Waypoints ******************/
 
-    $('.wp1').waypoint(function() {
-            $('.wp1').addClass('animated fadeInLeft');
-    }, {
-            offset: '75%'
-    });
-    $('.wp2').waypoint(function() {
-            $('.wp2').addClass('animated fadeInUp');
-    }, {
-            offset: '75%'
-    });
-    $('.wp3').waypoint(function() {
-            $('.wp3').addClass('animated fadeInDown');
-    }, {
-            offset: '55%'
-    });
-    $('.wp4').waypoint(function() {
-            $('.wp4').addClass('animated fadeInDown');
-    }, {
-            offset: '75%'
-    });
-    $('.wp5').waypoint(function() {
-            $('.wp5').addClass('animated fadeInUp');
-    }, {
-            offset: '75%'
-    });
-    $('.wp6').waypoint(function() {
-            $('.wp6').addClass('animated fadeInDown');
-    }, {
-            offset: '75%'
-    });
+    // $('.wp1').waypoint(function() {
+    //         $('.wp1').addClass('animated fadeInLeft');
+    // }, {
+    //         offset: '75%'
+    // });
+    // $('.wp2').waypoint(function() {
+    //         $('.wp2').addClass('animated fadeInUp');
+    // }, {
+    //         offset: '75%'
+    // });
+    // $('.wp3').waypoint(function() {
+    //         $('.wp3').addClass('animated fadeInDown');
+    // }, {
+    //         offset: '55%'
+    // });
+    // $('.wp4').waypoint(function() {
+    //         $('.wp4').addClass('animated fadeInDown');
+    // }, {
+    //         offset: '75%'
+    // });
+    // $('.wp5').waypoint(function() {
+    //         $('.wp5').addClass('animated fadeInUp');
+    // }, {
+    //         offset: '75%'
+    // });
+    // $('.wp6').waypoint(function() {
+    //         $('.wp6').addClass('animated fadeInDown');
+    // }, {
+    //         offset: '75%'
+    // });
     
     //img liquid
     // $('.img_fill').imgLiquid({fill: true});
--- app/assets/javascripts/global_effect.js.coffee
+++ app/assets/javascripts/global_effect.js.coffee
@@ -4,6 +4,9 @@ class Global
     @imgFill()
     @checkCustomize()
     @selectCtm()
+    @smoothScroll()
+    @optionBtn()
+    @wayPoints()
 
   dropdownNav: ->
     $('.expand_nav_point').on "click", ->
@@ -36,6 +39,60 @@ class Global
     }
     for selector of opts
       $(selector).chosen(opts[selector])
+
+  smoothScroll: ->
+    $('a[href*=#]:not([href=#])').on "click", ->
+      if location.pathname.replace(/^\//, '') is this.pathname.replace(/^\//, '') && location.hostname is this.hostname
+        target = $(this.hash)
+        target = target.length ? target : $("[name=#{this.hash.slice(1)}]")
+        if target.length && $(window).width() >= 640
+          $('html,body').animate {
+            scrollTop: target.offset.top + -60
+          }, 1000
+          return false
+        # else if target.length && $(window).width() <= 640
+        #   $("html,body").animate {
+        #     scrollTop: target.offset().top
+        #   }, 1000
+        #   return false
+        #   // for mobile screens
+
+  optionBtn: ->
+    $(".option_drop span").on "click", ->
+      $(this).parent().find("ul").slideToggle()
+
+  wayPoints: ->
+    $(".wp1").waypoint ->
+      $('wp1').addClass "animate fadeInLeft"
+    , {
+      offset: "75%"
+    }
+    $(".wp2").waypoint ->
+      $('wp2').addClass "animate fadeInUp"
+    , {
+      offset: "75%"
+    }
+    $(".wp3").waypoint ->
+      $('wp3').addClass "animate fadeInDown"
+    , {
+      offset: "55%"
+    }
+    $(".wp4").waypoint ->
+      $('wp4').addClass "animate fadeInDown"
+    , {
+      offset: "75%"
+    }
+    $(".wp5").waypoint ->
+      $('wp5').addClass "animate fadeInUp"
+    , {
+      offset: "75%"
+    }
+    $(".wp6").waypoint ->
+      $('wp6').addClass "animate fadeInDown"
+    , {
+      offset: "75%"
+    }
+
 
 $(document).on "page:load ready", ->
   new Global()
