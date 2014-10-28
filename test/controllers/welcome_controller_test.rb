aff3b625770b54b6b89515f59d76fd61d97280c5
peterzd <zdsunshine0640@gmail.com>
2014-10-28 11:33:29 +0800

    [test]make the coverage for tests more
    Note: work on the model/user_test.rb

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -5,4 +5,12 @@ describe WelcomeController do
     get :blog
     assert_response :success
   end
+
+  describe "GET support" do
+    it "shows the user a new support form" do
+      get :support
+      assigns[:support].wont_be_nil
+      assert_response :success
+    end
+  end
 end
