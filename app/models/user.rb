b1f6bd26993f8e16f1312f79e9d18c8f01606703
peterzd <zdsunshine0640@gmail.com>
2014-11-14 14:42:14 +0800

    [feature]add authentication_token to users for API token

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_save :ensure_authentication_token
+
+  def ensure_authentication_token
+    self.authentication_token ||= generate_authentication_token
+  end
+
   def admin?
     instance_of? AdminUser
   end
@@ -103,6 +109,14 @@ class User < ActiveRecord::Base
 
   def my_pending_invitations
     received_invitations.where status: Invitation::STATUS[:pending]
+  end
+  
+  private
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112081331) do
+ActiveRecord::Schema.define(version: 20141114063526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,8 +125,10 @@ ActiveRecord::Schema.define(version: 20141112081331) do
     t.integer  "cover_image_id"
     t.string   "type"
     t.integer  "cover_photo_id"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
   add_index "users", ["cover_image_id"], name: "index_users_on_cover_image_id", using: :btree
   add_index "users", ["cover_photo_id"], name: "index_users_on_cover_photo_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
