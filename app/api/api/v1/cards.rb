79a96ac2fa0175e7fe486832bf1d2578bc4999d7
peterzd <zdsunshine0640@gmail.com>
2014-11-14 15:16:00 +0800

    [feature]introduce devise authentication for API
    Note: every request should add `access_token` params if request for some
    user-sensitive resources

--- app/api/api/v1/cards.rb
+++ app/api/api/v1/cards.rb
@@ -4,9 +4,30 @@ module API
       version 'v1'
       format :json
 
+      helpers do
+        def warden
+          env['warden']
+        end
+
+        def authenticated
+          return true if warden.authenticated?
+          params[:access_token] && @user = User.find_by_authentication_token(params[:access_token])
+        end
+
+        def current_user
+          warden.user || @user
+        end
+
+        def authenticate!
+          error!("401 Unauthorized", 401) unless authenticated
+        end
+      end
+
+
       resources :cards do
         desc "returns all cards"
         get do
+          authenticate!
           Album.all
         end
 
---
+++ app/api/api/v1/users.rb
@@ -0,0 +1,8 @@
+module API
+  module V1
+    class Users < Grape::API
+      version 'v1'
+      format :json
+    end
+  end
+end
