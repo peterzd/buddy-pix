ca8a5695b10dace46e236fd406662dfdd2b286c8
peterzd <zdsunshine0640@gmail.com>
2014-11-21 21:32:54 +0800

    [chore]comments can have image

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :commenter, class_name: "User", foreign_key: :commenter_id
   belongs_to :commentable, class_name: "Photo", foreign_key: :commentable_id
+
+  has_one :image, as: :imageable, dependent: :destroy
+
+  def picture_url(format=:original)
+    return "" if image.nil?
+    image.picture.url format
+  end
 end
 
