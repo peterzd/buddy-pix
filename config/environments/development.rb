11bd3a7a634917310b44983c76fd11b58a5ca3eb
peterzd <zdsunshine0640@gmail.com>
2014-11-24 20:28:53 +0800

    [chore]change domain name from .com to .net

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'buddypix.com',
+    domain:               'buddypix.net',
     user_name:            ENV["EMAIL_NAME"],
     password:             ENV["EMAIL_PWD"],
     authentication:       'plain',
