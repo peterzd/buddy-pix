be19cc2efb9bfda0a8e4bc66b2eddbe1d54ae5cf
peterzd <zdsunshine0640@gmail.com>
2014-11-29 14:52:55 +0800

    [feature]create InvitationToken model to store the token

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,27 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token, only: [:destroy]
 
+  def new
+    if params[:invitation_token]
+      @invitation_token = params[:invitation_token]
+    end
+    super
+  end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+
+    if params[:invitation_token]
+      token = params[:invitation_token]
+      invitation_token = InvitationToken.find_by token: token
+      action = invitation_token.action
+      current_user.joins_album action
+      redirect_to card_path action
+    else
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    end
+  end
 end
---
+++ app/models/invitation_token.rb
@@ -0,0 +1,17 @@
+class InvitationToken < ActiveRecord::Base
+  MODE = { email: "email", sms: "sms" }
+
+  belongs_to :inviter, class_name: "User"
+  belongs_to :action, polymorphic: true
+
+  validates :invitation_mode, inclusion: { in: %w(email sms),
+    message: "%{value} is not a valid type" }
+
+  class << self
+    def generate_token(options)
+      token = SecureRandom.base64(10)
+      create options.merge(token: token)
+      token
+    end
+  end
+end
