0352780e694ac30779b032b12fc2a287e469ec6c
peterzd <zdsunshine0640@gmail.com>
2014-11-30 10:41:25 +0800

    [chore]use real host address to send invitation email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,11 +1,12 @@
 class InvitationMailer < ActionMailer::Base
   default from: "buddypix.info@gmail.com"
 
-  def invite(sender, to_url, content=nil, card, token)
+  def invite(sender, to_url, content=nil, card, token, request)
+    host = request.host_with_port
     @user = sender
     @content = content || "I would like to invite you on BuddyPix to check our latest Card and Images. Please click on this link to access:"
     @card = card
-    @link_url = "http://localhost:3000/users/sign_in?invitation_token=#{token}"
+    @link_url = "http://#{host}/users/sign_in?invitation_token=#{token}"
 
     mail(to: to_url, subject: "Invite you to BuddyPix")
   end
