70a85a42050afe38add70f60aeb12182e579536c
peterzd <zdsunshine0640@gmail.com>
2014-12-09 23:18:26 +0800

    [chore]expose likes, comments for cards

--- app/api/api/entities/card.rb
+++ app/api/api/entities/card.rb
@@ -1,11 +1,26 @@
 module API
   module Entities
     class Card < Grape::Entity
-
       expose :name, :private, :caption, :created_at, :last_name, :hidden
       expose :creator, using: API::Entities::User, unless: { type: :with_cards }
       expose :cover_image, using: API::Entities::Image
       expose :photos, using: API::Entities::Photo
+      expose :likes
+      expose :comments
+      expose :followers_count
+
+      private
+      def likes
+        object.total_likes
+      end
+
+      def comments
+        object.total_comments
+      end
+
+      def followers_count
+        object.followers.count
+      end
     end
   end
 end
