d442107fb52bbcfee4c34d68d5591002819e8a04
peterzd <zdsunshine0640@gmail.com>
2014-12-10 17:15:09 +0800

    [chore]user user input content replace default one

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,8 +4,7 @@ class InvitationMailer < ActionMailer::Base
   def invite(sender, to_url, content=nil, card, token, email_host)
     host = email_host
     @user = sender
-    @default_content = "Hi, #{sender.user_name} has sent you an invitation to join BuddyPix, a new interest-based social platform.  You can click on the link below to accept their invitation and create an account"
-    @content = content
+    @content = content || "Hi, #{sender.user_name} has sent you an invitation to join BuddyPix, a new interest-based social platform.  You can click on the link below to accept their invitation and create an account"
     @card = card
     @link_url = "http://#{host}/users/sign_in?invitation_token=#{token}"
 
