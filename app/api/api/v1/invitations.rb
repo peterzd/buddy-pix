030e7dfa5b2f2845167c02c607597486167526a8
peterzd <zdsunshine0640@gmail.com>
2014-12-18 23:28:40 +0800

    [feature]invite users via email

--- app/api/api/v1/invitations.rb
+++ app/api/api/v1/invitations.rb
@@ -19,7 +19,25 @@ module API
           end
           present :status, "true"
         end
-      end
+
+        desc "invites others in app contacts"
+        params do
+          requires :access_token, type: String, desc: "the token of the user"
+          requires :card_id, type: String, desc: "the card invited to"
+          requires :emails, type: Array, desc: "invited users"
+          optional :content, type: String, desc: "content of the email"
+        end
+        post :email do
+          authenticate!
+          card = Album.find params[:card_id].to_i
+          emails = params[:emails]
+          email_host = request.host_with_port
+          content = params[:content]
+
+          InvitationsService.send_email(current_user, card, emails, content, email_host)
+          present :status, "true"
+        end
+      end # end of resource
     end
   end
 end
