72bba49cb15409701a29fca7c771d2ceedb00be5
peterzd <zdsunshine0640@gmail.com>
2014-12-20 22:33:02 +0800

    [refactor]search cards batch

--- app/api/api/v1/cards.rb
+++ app/api/api/v1/cards.rb
@@ -1,6 +1,3 @@
-Kaminari::Hooks.init
-Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
-
 module API
   module V1
     class Cards < Grape::API
@@ -33,7 +30,7 @@ module API
         end
         post "search_cards" do
           authenticate!
-          cards = Album.search(params[:search_params]).page(params[:page].to_i).per(1).records
+          cards = AlbumsService.search_cards_batch(params[:search_params], params[:page].to_i)
           present :status, "true"
           present :cards, cards, with: API::Entities::Card
         end
