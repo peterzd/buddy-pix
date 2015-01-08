4d64ceea83279354d46ab9f6de8f2bf733a6964d
peterzd <zdsunshine0640@gmail.com>
2015-01-09 00:01:12 +0800

    [chore]authentication for admin pages

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,9 @@
 class Admin::ApplicationController < ApplicationController
+  before_action :authenticate_user
+
+  protected
+  def authenticate_user
+    redirect_to root_path unless current_user.admin?
+  end
 
 end
