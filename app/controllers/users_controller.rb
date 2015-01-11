85d87de301d7a6bac01a5954b065bb798314e4bf
peterzd <zdsunshine0640@gmail.com>
2015-01-11 23:28:37 +0800

    [chore]test for message flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,8 @@ class UsersController < ApplicationController
   end
 
   def update_account_settings
+    @messages = Hash.new
+    @messages[:success] = Hash.new
     if params[:commit] == "Delete Account"
       user = current_user
       sign_out_and_redirect current_user
@@ -51,25 +53,17 @@ class UsersController < ApplicationController
       user.phone_number = user_params[:phone_number]
       if user.changed_attributes.any?
         user.save
-        flash[:success] = "profile updated"
+        @messages[:success].merge!({ profile: "profile updated" })
       end
 
       if user_params[:profile_cover]
         UsersService.new(user).update_profile_cover(user_params, :profile_cover)
-        if flash[:success]
-          flash[:success] << "\nprofile cover updated "
-        else
-          flash[:success] = "\nprofile cover updated "
-        end
+        @messages[:success].merge!({ profile_cover: "profile cover updated " })
       end
 
       if user_params[:cover_photo]
         UsersService.new(user).update_cover_photo(user_params, :cover_photo)
-        if flash[:success]
-          flash[:success] << "\ncover photo updated "
-        else
-          flash[:success] = "\ncover photo updated "
-        end
+        @messages[:success].merge!({ cover_photo: "cover photo updated " })
       end
 
       current_pwd = params[:current_password]
@@ -78,12 +72,13 @@ class UsersController < ApplicationController
 
       if current_pwd.present? && new_pwd.present? && confirm_pwd.present?
         if process_password(current_pwd, new_pwd, confirm_pwd)
-          flash[:info] = "password updated"
+          @messages[:success].merge!({ password: "password updated" })
         else
           flash[:danger] = "can not update password"
         end
       end
 
+      flash[:hash] = @messages
       redirect_to dashboard_account_settings_path
     end
   end
