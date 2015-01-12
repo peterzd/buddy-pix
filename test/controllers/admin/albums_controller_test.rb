fd3f5cf9cdf1d0dc5e7573cf789fb40f321295f8
peterzd <zdsunshine0640@gmail.com>
2015-01-12 21:10:13 +0800

    [chore]make tests pass

--- test/controllers/admin/albums_controller_test.rb
+++ test/controllers/admin/albums_controller_test.rb
@@ -4,6 +4,10 @@ describe Admin::AlbumsController do
   helper_objects
 
   describe "DELETE destroy" do
+    before do
+      sign_in admin
+    end
+
     it "destroys the card" do
       album
       delete :destroy, id: album
