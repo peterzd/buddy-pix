8d7b446fec9678cc79f29e7e8b26a8b18712e740
peterzd <zdsunshine0640@gmail.com>
2015-01-21 21:50:39 +0800

    [chore]fix bug for sign in/up notification

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,21 +1,22 @@
 class CustomFailure < Devise::FailureApp
-    def redirect_url
-      #return super unless [:worker, :employer, :user].include?(scope) #make it specific to a scope
-      user = User.find_by email: params[:user][:email]
-      if user and (user.confirmed_at.nil?)
-        flash[:danger] = "Please confirm your account in your email first"
-      else
-        flash[:danger] = "email or password error"
-      end
-      new_user_session_path
+  def redirect_url
+    #return super unless [:worker, :employer, :user].include?(scope) #make it specific to a scope
+    user = User.find_by email: params[:user][:email]
+    flash.clear
+    if user and (user.confirmed_at.nil?)
+      flash[:danger] = "Please confirm your account in your email first"
+    else
+      flash[:danger] = "email or password error"
     end
+    new_user_session_path
+  end
 
-    # You need to override respond to eliminate recall
-    def respond
-      if http_auth?
-        http_auth
-      else
-        redirect
-      end
+  # You need to override respond to eliminate recall
+  def respond
+    if http_auth?
+      http_auth
+    else
+      redirect
     end
   end
+end
